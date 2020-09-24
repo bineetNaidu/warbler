1ffcebb72dfd92bc051840c802caa1a7f315064a
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-24 14:37:27 +0530

    initializing app Component

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { configStore } from "../store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // STATICS
 
 function App() {
+  // STATES
+
+  // HOOKS && CONTEXT
+  const store = configStore();
+
+  // FUNCTIONS
+
   return (
-    <div className="App">
-      <h1>Let Build WArbler Client Side...</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div>Hello Warbler....</div>
+      </Router>
+    </Provider>
   );
 }
 
