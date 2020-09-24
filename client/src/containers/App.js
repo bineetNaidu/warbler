3d9bf07606eb859fbf2a2c61be3b6b21b10e90ff
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-24 14:00:46 +0530

    setups && cleanups

---
+++ client/src/containers/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// STATICS
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let Build WArbler Client Side...</h1>
+    </div>
+  );
+}
+
+export default App;
